62e2b73be5af365d2c9f91e089b65cc2645d3db0
slug-enjoyer <81844498+slug-enjoyer@users.noreply.github.com>
2023-07-03 22:54:25 -0700

    Basic structure added and plan written in comments
    Got the basic html setup, put in a reference to the
    script.js. wrote out a basic plan in the comments on
    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+//This script should play a round of Rock, Paper, Scissors
+//(or Roshambo) through the console. It will need a function
+//to run the computers choice, which will be decided with a
+//random number. The computer choice should be assigned to a 
+//variable called "compChoice". The user will then input their
+//decision, which will then be assigned to a variable called
+//"userChoice". We could send the user choice thru a function
+//to uniformize it, so that it can be referenced later when
+//the game runs the two choices against each other.
+//The other choice is to make the game part more robust. 
+//I think I would rather have it uniformed.
+
+
+
+function getComputerChoice (){
+
+}
